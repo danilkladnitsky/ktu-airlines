b4ed69a2f7204f3ca2d5aaf95ef76ad3b22bec3c
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-04-09 21:24:54 +0300

    init frontend

---
+++ frontend/src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+ReactDOM.render(
+    <React.StrictMode>
+        "Hello"
+    </React.StrictMode>,
+    document.getElementById("root")
+);
