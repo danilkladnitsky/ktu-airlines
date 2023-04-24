1a1da9529b9fa3a5b000ac2e1f6d53e0e4575f6b
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-04-24 12:02:31 +0300

    fix

--- frontend/src/pages/BuyTicketPage/components/HotelList/Hotel.tsx
+++ frontend/src/pages/BuyTicketPage/components/HotelList/Hotel.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Group, Image, List, Rating, Stack, Text, Title } from '@mantine/core';
+import { Button, Card, Flex, Group, Image, List, Rating, Stack, Text, Title } from '@mantine/core';
 import { useAppStore } from 'store';
 
 import { Hotel as HotelType } from 'domain/hotel';
@@ -16,18 +16,17 @@ export const Hotel = ({ hotel }: Props) => {
   const { description, name, stars, thumbnailUrl, active } = hotel;
 
   return (
-    <Card shadow={'sm'} padding="xl" radius={'lg'}>
-      <Card.Section>
-        <Image
-          src={thumbnailUrl}
-          height={160}
-          alt="Norway"
-        />
-      </Card.Section>
-      <Group position="apart" mt="md" mb="xs">
-        <Title order={4}>{name}</Title>
-      </Group>
-      <Stack className={styles.hotelContent}>
+    <Card shadow={'sm'} padding="xl" radius={'lg'} className={styles.hotel}>
+      <Stack>
+        <Card.Section className={styles.pic}>
+          <Image
+            src={thumbnailUrl}
+            alt="Norway"
+          />
+        </Card.Section>
+        <Group position="apart" mt="md" mb="xs">
+          <Title order={4}>{name}</Title>
+        </Group>
         <Rating value={stars} readOnly size={'xs'} />
         <Text size="sm">
           <Title order={5}>
@@ -43,10 +42,10 @@ export const Hotel = ({ hotel }: Props) => {
           <List.Item>Питание</List.Item>
           <List.Item>SPA</List.Item>
         </List>
-        <Button className={styles.selectBtn} variant="light" color="blue" mt="md" radius="md" onClick={incrementFormId} disabled={!active}>
-            Выбрать
-        </Button>
       </Stack>
+      <Button className={styles.selectBtn} variant="light" color="blue" mt="md" radius="md" onClick={incrementFormId} disabled={!active}>
+            Выбрать
+      </Button>
     </Card>
   );
 };
